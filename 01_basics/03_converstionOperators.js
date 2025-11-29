172ef3253a8061f008311e28cd60a9818784b66d
Zorain Jawaid <zeethedev20@gmail.com>
2025-11-29 15:10:28 +0000

    converstion of datatypes

---
+++ 01_basics/03_converstionOperators.js
@@ -0,0 +1,40 @@
+// let score = 28;
+// let scoreNew = "22aaa";
+// let scoreNull = null;
+// let scoreUndefined = undefined;
+
+// console.log(typeof score);
+// console.log(typeof (scoreNew));
+
+// let valueInNumber = Number(scoreNew);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+//console.log(typeof scoreNull);
+//console.log(typeof (scoreUndefined));
+
+// let valueInNumberNull = Number(scoreNull);
+//console.log(typeof (valueInNumberNull));
+
+// let valueInNumberUndefined = Number(scoreUndefined);
+//console.log(typeof (valueInNumberUndefined));
+
+//--Notes--
+// 33 => "33"  
+// "33aaa" => NaN
+// true => 1 false => 0
+
+// let isLoggedIn = " ";
+// let boolenIsloggedIn = Boolean(isLoggedIn);
+// console.log (boolenIsloggedIn);
+
+//--Notes--
+// 1 => true; 0 => false;
+// "" = false;
+// "zee" => true; 
+
+
+let Value = 33;
+let stringValue = String(Value);
+
+console.log (typeof stringValue);
